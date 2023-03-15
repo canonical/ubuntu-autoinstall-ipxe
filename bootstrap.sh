55b04ae082c89491c97d658245f131e9d40649e9
Ken VanDine <ken.vandine@canonical.com>
2023-03-14 20:37:48 -0400

    Save a backup of fstab

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ patch -p1 < $checkout/etc/squid-deb-proxy/squid-deb-proxy.conf.diff
 cd /etc/apache2/sites-available/
 patch -p1 < $checkout/etc/apache2/sites-available/000-default.conf.diff
 
+cp /etc/fstab /etc/fstab.orig
 echo "/var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/ubuntu-22.04.2-live-server-amd64.iso /var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/iso iso9660 loop,ro,auto 0 0" >> /etc/fstab
 
 echo "Bootstrap complete"
