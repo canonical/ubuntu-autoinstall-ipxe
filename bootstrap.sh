e25f44795fafee87ebfc951da2f17fe160d02661
Ken VanDine <ken.vandine@canonical.com>
2023-03-14 20:22:57 -0400

    Fixed apache patch location

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ checkout=$PWD
 cd /etc/squid-deb-proxy/
 patch -p1 < $checkout/etc/squid-deb-proxy/mirror-dstdomain.acl.d/10-default.diff
 patch -p1 < $checkout/etc/squid-deb-proxy/squid-deb-proxy.conf.diff
-cd /etc/apache2/sites-enabled/
-patch -p1 < $checkout/etc/apache2/sites-enabled/000-default.conf.diff
+cd /etc/apache2/sites-available/
+patch -p1 < $checkout/etc/apache2/sites-available/000-default.conf.diff
 echo "Bootstrap complete"
 cd $checkout
