9f5039f4c9c42e1dde0f0f26fc83b33efc5d949f
Ken VanDine <ken.vandine@canonical.com>
2023-03-17 11:24:37 -0400

    Added download of the server iso to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,9 @@ cd /etc/apache2/sites-available/
 patch -p1 < $checkout/etc/apache2/sites-available/000-default.conf.diff
 
 cp /etc/fstab /etc/fstab.orig
-echo "/var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/ubuntu-22.04.2-live-server-amd64.iso /var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/iso iso9660 loop,ro,auto 0 0" >> /etc/fstab
+
+wget -O $checkout/ubuntu/22.04/server-22.04.2.iso https://mnvoip.mm.fcix.net/ubuntu-releases/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
+echo "/var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/server-22.04.2.iso /var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/iso iso9660 loop,ro,auto 0 0" >> /etc/fstab
 
 echo "Bootstrap complete"
 cd $checkout
