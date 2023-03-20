5473855ccc88ee122a169f953c00a9de647a7a7e
tvdkcompudopt <126183019+tvdkcompudopt@users.noreply.github.com>
2023-03-20 10:44:31 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,7 @@ patch -p1 < $checkout/etc/tftpd.remap.diff
 
 cp /etc/fstab /etc/fstab.orig
 
+cd /var/www/html/
 wget -O $checkout/ubuntu/22.04/server-22.04.2.iso https://mnvoip.mm.fcix.net/ubuntu-releases/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
 echo "/var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/server-22.04.2.iso /var/www/html/ubuntu-autoinstall-iso/ubuntu/22.04/iso iso9660 loop,ro,auto 0 0" >> /etc/fstab
 
